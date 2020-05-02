6005c01d5a5bf765d50fd67321fbd9d78205d3c4
celal <c.cagriyilmaz@gmail.com>
2020-05-02 14:41:29 +0300

    React Build Put inside Java Resources

---
+++ src/main/resources/static/precache-manifest.0226801553ed1a67bd8cf2be2b070417.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9788ef444fc202405a69eafb0ab903f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee1cb2ad924ec65b04bf",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0c97bf81831491a59345",
+    "url": "/static/css/main.49da9324.chunk.css"
+  },
+  {
+    "revision": "ee1cb2ad924ec65b04bf",
+    "url": "/static/js/2.4bb6a308.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.4bb6a308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c97bf81831491a59345",
+    "url": "/static/js/main.36ba288b.chunk.js"
+  },
+  {
+    "revision": "c081e2f36df4981bab2b",
+    "url": "/static/js/runtime-main.8b4b340c.js"
+  },
+  {
+    "revision": "c428d1f33cece1302e45246fb90d3bb5",
+    "url": "/static/media/picture2.c428d1f3.png"
+  }
+]);
